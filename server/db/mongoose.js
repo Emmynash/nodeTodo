47fbd8c942e6dae761387c72b5effd5f86d868a4
E Akita <emmy4nash@gmail.com>
2019-01-22 16:18:23 +0100

    heroku set up

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/TodoAPI"
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoAPI";
 
 mongoose.Promise = global.Promise;
 
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
