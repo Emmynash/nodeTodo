a0751737be3d1eb7b9a3bea31c64db4b20be40ae
E Akita <emmy4nash@gmail.com>
2019-02-03 10:55:35 +0100

    Add Post users login route

--- server/models/user.js
+++ server/models/user.js
@@ -76,6 +76,33 @@ UserSchema.statics.findByToken = function(token) {
 
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({ email })
+        .then((user) => {
+            // console.log(user);
+            if (!user) {
+                return Promise.reject();
+            }
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    // console.log(`${password} equals: ${user.password}`)
+                    if (res) {
+                        // console.log(res);
+                        resolve(user);
+                        // console.log(user);
+                    } else {
+                        // console.log(err);
+                        reject(err);
+                    }
+
+                })
+
+            });
+        })
+}
+
 UserSchema.pre('save', function(next) {
     var user = this;
     if (user.isModified("password")) {
--- server/server.js
+++ server/server.js
@@ -133,6 +133,22 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 }, ((e) => console.log(e)));
 
+// Post users/login
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password'])
+
+    User.findByCredentials(body.email, body.password).then((result) => {
+        return result.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(result);
+        });
+        console.log(result);
+    }).catch((err) => {
+        res.status(400).send(err);
+        // console.log(err);
+    });
+
+});
+
 
 app.listen(port, (error) => {
     if (error) {
